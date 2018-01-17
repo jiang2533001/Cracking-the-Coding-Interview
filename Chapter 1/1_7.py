bb87fce6f5da52bee2808ad9f4219678fdc8fb91
jiang2533001 <jiang2533001@gmail.com>
2018-01-16 19:17:50 -0800

    change 1.7

--- Chapter 1/1_7.py
+++ Chapter 1/1_7.py
@@ -1,11 +1,21 @@
-length = 4
-matrix = [range(length) for x in range(length)]
+import random
 
-for i in range(length-1):
-    temp = matrix[0][i]
-    matrix[0][i] = matrix[length-1-i][0]
-    matrix[length-1-i][0] = matrix[length-1][length-1-i]
-    matrix[length-1][length-1-i] = matrix[i][length-1]
-    matrix[i][length-1] = temp
+def main():
+    length = 4
+    matrix = [[random.randint(0, 15) for x in range(length)] for y in range(length)]
+    
+    print matrix
+    print solution(matrix, length)
 
-print matrix
+def solution(matrix, length):
+    for i in range(length-1):
+        temp = matrix[0][i]
+        matrix[0][i] = matrix[length-1-i][0]
+        matrix[length-1-i][0] = matrix[length-1][length-1-i]
+        matrix[length-1][length-1-i] = matrix[i][length-1]
+        matrix[i][length-1] = temp
+
+    return matrix
+
+if __name__ == '__main__':
+    main()

506d9ca889f60e48a64f3ccdd2e652a4b833f2eb
jiang2533001 <jiang2533001@gmail.com>
2018-01-18 17:19:37 -0800

    create stack structure

---
+++ Chapter 2/stack.py
@@ -0,0 +1,27 @@
+class Stack(object):
+    def __init__(self):
+        self.head = None
+        self.list = []
+        self.size = 0
+
+    def is_empty(self):
+        if self.size == 0:
+            return True
+        else:
+            return False
+
+    def push(self, val):
+        self.list.append(val)
+        self.head = self.size
+        self.size += 1
+
+    def pop(self):
+        if not self.is_empty():
+            val = self.list[self.head]
+            del self.list[self.head]
+            self.size -= 1
+            self.head = self.size - 1 
+            return val
+
+    def front(self):
+        return self.list[self.head]

d61ff96d0bd70e81b669ba88a3d7b042eb94ce7a
jiang2533001 <jiang2533001@gmail.com>
2018-01-21 14:05:55 -0800

    complete 4_1

--- Chapter 4/4_1.py
+++ Chapter 4/4_1.py
@@ -1,4 +1,6 @@
 from graph import Graph, Node
+from stack import Stack
+from queue import Queue
 
 def main():
     graph = Graph()
@@ -13,14 +15,46 @@ def main():
     graph.connect_vertices(graph.vertices_set[3], graph.vertices_set[4])
     graph.connect_vertices(graph.vertices_set[0], graph.vertices_set[5])
 
-    print DFS()
-    print BFS()
+    print DFS(graph.vertices_set[3], graph.vertices_set[0])
+    print BFS(graph.vertices_set[3], graph.vertices_set[0])
 
-def DFS():
+def DFS(start, end):
+    stack = Stack()    
+    stack.push(start)
+    
+    visited_set = {}
+    visited_set[start.label] = True
 
+    while not stack.is_empty():
+        current = stack.pop()
+        for i in range(current.neighbors_num):
+            node = current.neighbors[i]
+            if not visited_set.has_key(node.label):
+                visited_set[node.label] = True
+                if node.label == end.label:
+                    return True
+                else:
+                    stack.push(node)
+    return False
 
-def BFS():
+def BFS(start, end):
+    queue = Queue()
+    queue.add(start)
 
+    visited_set = {}
+    visited_set[start.label] = True
+
+    while not queue.is_empty():
+        current = queue.remove()
+        for i in range(current.neighbors_num):
+            node = current.neighbors[i]
+            if not visited_set.has_key(node.label):
+                visited_set[node.label] = True
+                if node.label == end.label:
+                    return True
+                else:
+                    queue.add(node)
+    return False
 
 if __name__ == '__main__':
     main()

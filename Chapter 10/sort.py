96342bdde4beea6dde814317c54f205889f8f692
jiang2533001 <jiang2533001@gmail.com>
2018-01-31 15:23:06 -0800

    complete quick sort

--- Chapter 10/sort.py
+++ Chapter 10/sort.py
@@ -19,13 +19,35 @@ def selection(l):
 
 def insert(l):
     for i in range(1, len(l)):
-            temp = l[i]
-            for j in range(i-1, -1, -1):
-                if l[j] > temp:
-                    l[i], l[i+1] = l[i+1], l[i]
-                else:
-                    break
+        temp = l[i]
+        for j in range(i-1, -1, -1):
+            if l[j] > temp:
+                l[i], l[i+1] = l[i+1], l[i]
+            else:
+                break
     return l
+
+def quick(l):
+    return quick_sort(l, 0, len(l)-1)
+
+def quick_sort(l, left, right):
+    if left < right:
+    pi = partition(l, left, pi-1)
+    
+    quick_sort(l, left, pi-1)
+    quick_sort(l, pi+1, right)
+
+def partition(l, left, right):
+    i = left - 1
+    piovt = l[right]:
+
+    for j in range(left, high):
+        if l[j] <= pivot:
+            i = i + 1
+            l[i], l[j] = l[j], l[i]
+
+    l[i+1], l[right] = l[right], l[i+1]
+    return i + 1
 
 def merge(l):
 
